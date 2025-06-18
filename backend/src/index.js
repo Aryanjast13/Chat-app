d0018bce53edcd57a8df7d5865687be10466dbfe
Aryanjast13 <aryanjast05@gmail.com>
2025-06-18 09:54:47 +0530

    first commit

---
+++ backend/src/index.js
@@ -0,0 +1,52 @@
+import cookieParser from "cookie-parser";
+import cors from "cors";
+import dotenv from "dotenv";
+import express from "express";
+
+import path from "path";
+import { fileURLToPath } from "url";
+
+import { connectDB } from "./lib/db.js";
+
+import { app, server } from "./lib/socket.js";
+import authRoutes from "./routes/auth.route.js";
+import messageRoutes from "./routes/message.route.js";
+
+dotenv.config();
+
+const PORT = process.env.PORT;
+// Required for __dirname in ES Module
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+// Serve static frontend
+app.use(express.static(path.join(__dirname, "frontend")));
+
+app.use(express.json());
+app.use(cookieParser());
+app.use(
+  cors({
+    origin: "http://localhost:5173",
+    credentials: true,
+  })
+);
+
+app.use("/api/auth", authRoutes);
+app.use("/api/messages", messageRoutes);
+
+// All other routes → index.html
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "frontend", "index.html"));
+});
+
+try {
+  server.listen(PORT, () => {
+    console.log("server is running on PORT:" + PORT);
+    connectDB();
+  });
+  
+}
+catch (error) {
+  console.log(error.message);
+}
+
